a3948bd0ab8867c0c2ce1ea4966a3c87ca13ab3e
valeti00 <57063966+valeti00@users.noreply.github.com>
2019-12-20 16:01:05 +0300

    Add files via upload

---
+++ zadacha15/zadacha15.cpp
@@ -0,0 +1,33 @@
+﻿// zadacha15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iterator>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	set<int> a;
+
+	for (int i = 0; i < n; ++i) {
+		int t;
+		cin >> t;
+
+		a.insert(t);
+
+		int j = 0;
+
+		set<int> b;
+
+		for (auto it = a.cbegin(); it != a.cend() && j < 5; ++it, ++j)
+			b.insert(*it);
+
+		copy(b.crbegin(), b.crend(), ostream_iterator<int>(cout, " "));
+
+		cout << '\n';
+	}
+}
---
+++ zadacha18/zadacha18.cpp
@@ -0,0 +1,42 @@
+﻿// zadacha18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+const int n = 20;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	srand(time(NULL));
+
+	int mass[n], max, min;
+
+	cout << "Числа: ";
+	for (int r = 0; r < n; r++)
+	{
+		mass[r] = rand() % 202 - 101;
+		cout << mass[r] << " ";
+	}
+	cout << endl;
+
+	max = mass[0];
+	min = mass[0];
+	for (int r = 1; r < n; r++)
+	{
+		if (max < mass[r]) max = mass[r];
+		if (min > mass[r]) min = mass[r];
+	}
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
+
+	return 0;
+}
+
+
+
