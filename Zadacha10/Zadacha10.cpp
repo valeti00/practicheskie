8d737cf812558d32774a809f4237f8e3f2719057
valeti00 <57063966+valeti00@users.noreply.github.com>
2019-12-20 15:49:59 +0300

    Add files via upload

---
+++ Zadacha10/Zadacha10.cpp
@@ -0,0 +1,18 @@
+﻿// Zadacha10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, n, res;
+	cin >> a >> n;
+	res = a;
+	for (int i = 1; i < n; i++) {
+		res = res*a;
+			}
+	cout << res;
+	return 0;
+
+}
---
+++ zadacha8/zadacha8.cpp
@@ -0,0 +1,43 @@
+﻿// zadacha8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double a, b, c;
+	char x;
+	cin >> a;
+	cin >> x;
+	cin >> b;
+
+	switch (x)
+	{
+	case('+'): c = a + b;
+	{ cout << "Ответ: " << c << '\n';
+	break;
+	}
+
+	case('-'): c = a - b;
+	{ cout << "Ответ: " << c << '\n';
+	break;
+	}
+
+	case('*'): c = a * b;
+	{ cout << "Ответ: " << c << '\n';
+	break;
+	}
+	case('/'): c = a / b;
+	{ cout << "Ответ: " << c << '\n';
+	break;
+	if (b == 0)
+		cout << "ОШИБКА";
+	}
+	cout << "Ответ=" << c << "\n";
+	}
+
+}
+
