1ca7203d1e4e0f91a8c6f9c5dc6071c6fa914e85
valeti00 <57063966+valeti00@users.noreply.github.com>
2019-12-20 15:58:02 +0300

    Add files via upload

---
+++ Zadacha11/Zadacha11.cpp
@@ -0,0 +1,25 @@
+﻿// Zadacha11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+// объявление функции нахождения n!
+void faktorial(int numb)// заголовок функции
+{
+	int rezult = 1; // инициализируем переменную rezult значением 1
+	for (int i = 1; i <= numb; i++) // цикл вычисления значения n!
+		rezult *= i; // накапливаем произведение в переменной rezult
+	cout << numb <<"! = " << rezult << endl; // печать значения n!
+}
+
+int main(int argc, char* argv[])
+{
+	int digit; // переменная для хранения значения n!
+	cout << "Enter number: ";
+	cin >> digit;
+	faktorial(digit);// запуск функции нахождения факториала
+	system("pause");
+	return 0;
+}
+
---
+++ Zadacha13/Zadacha13.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int c = 0;
+	long long a, x = 1;
+	cin >> a;
+	while (x <= a)
+	{
+		if (x <= a)
+			c++;
+		x *= 2;
+	}
+	cout << c;
+}
