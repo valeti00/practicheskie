1420f5849b2233c73def1df5e32c3752b99dac34
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 13:51:08 +0300

    Update zadacha8.cpp

--- zadacha8/zadacha8.cpp
+++ zadacha8/zadacha8.cpp
@@ -1,4 +1,4 @@
-﻿// zadacha8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// zadacha8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -9,18 +9,17 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	double a, b, c;
-	char x;
+	char x = '?';
 	cin >> a;
 	cin >> x;
 	cin >> b;
 
 	switch (x)
 	{
-	case('+'): c = a + b;
-	{ cout << "Ответ: " << c << '\n';
+	case '+': c = a + b;
+	cout << "Ответ: " << c << '\n';
 	break;
-	}
-
+	
 	case('-'): c = a - b;
 	{ cout << "Ответ: " << c << '\n';
 	break;
@@ -31,13 +30,18 @@ int main()
 	break;
 	}
 	case('/'): c = a / b;
-	{ cout << "Ответ: " << c << '\n';
+		if (b == 0) {
+			cout << "ОШИБКА";
+		}
+		else {
+			cout << "Ответ: " << c << '\n';
+		}
+	
 	break;
-	if (b == 0)
-		cout << "ОШИБКА";
+	
+	default:
+		cout << "Неверное значение" << "\n";
+	
 	}
-	cout << "Ответ=" << c << "\n";
-	}
-
 }
 
