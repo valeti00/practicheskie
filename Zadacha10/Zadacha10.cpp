b61f5ea23cb54439354cdc79a569cfeb2d907ee7
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 13:53:10 +0300

    Add files via upload

---
+++ Zadacha10/Zadacha10.cpp
@@ -0,0 +1,28 @@
+﻿// Zadacha10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, result;
+	int n;
+	cin >> a >> n;
+	result = 1;
+	for (int i = 1; i <= abs(n); i++)
+ {
+		result = result*a;
+	}
+	
+	if (n < 0) {
+
+		if (result == 0) 
+			cout << "ОШИБКА";
+		else
+		cout << 1.0 / result;
+
+	}
+	else 
+		cout << result;
+}
