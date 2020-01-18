10891fe0b731640c7e5111a588e92f218727ad16
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 13:56:17 +0300

    Add files via upload

---
+++ Zadacha11/Zadacha11.cpp
@@ -0,0 +1,17 @@
+﻿// Zadacha11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, result;
+	 
+	cin >> n;
+	result = 1;
+	for (int i = 1; i <= n; i++) {
+		result *= i;
+	}
+	cout << result;
+	return 0;
+}
