6559455b461afca6fb14aff3c44e56cffbf373c1
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 13:57:18 +0300

    Add files via upload

---
+++ Zadacha12/Zadacha12.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, i = 2;
+	cin >> a;
+
+	if (a > 0)
+	{
+
+		if (a == 1)
+		{
+			cout << "не простое и не составное";
+			return 0;
+		}
+
+		for (i; i < a; i++)
+		{
+			if (a % i == 0)
+			{
+				cout << "составное";
+				return 0;
+			}
+		}
+		cout << "простое";
+
+	}
+	else
+	{
+		cout << "Введено отрицательное число";
+	}
+}
