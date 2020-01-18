9fac55195989668d73bf5ef149017594b0ffc93a
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 15:19:47 +0300

    Add files via upload

---
+++ zadacha17/zadacha17.cpp
@@ -0,0 +1,27 @@
+﻿// zadacha17.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+double BMI(double weight, double height) {
+	height /= 100;
+	return weight / (height * height);
+}
+void vivodBMI(double BMI)
+{
+	if (BMI < 18.5) cout << "Underweight" << endl;
+	else if (18.5 <= BMI && BMI < 25.0) cout << "Normal weight" << endl;
+	else if (25.0 <= BMI && BMI < 30.0) cout << "Overweight" << endl;
+	else if (30.0 <= BMI) cout << "Obesity" << endl;
+}
+int main() {
+	setlocale(LC_ALL, "RUS");
+	double weight, height;
+	cout << "Введите Ваш вес и рост: " << endl;
+	cin >> weight >> height;
+	double bmi = BMI(weight, height);
+	vivodBMI(bmi);
+	cout << bmi;
+	return 0;
+}
