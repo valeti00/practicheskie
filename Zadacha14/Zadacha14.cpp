6072b3098cd3196fb3cd89f8afb4abc2918f11a3
valeti00 <57063966+valeti00@users.noreply.github.com>
2020-01-18 15:03:56 +0300

    Update Zadacha14.cpp

--- Zadacha14/Zadacha14.cpp
+++ Zadacha14/Zadacha14.cpp
@@ -1,23 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main()
 {
-	long long s, l1, l2, r1, r2, i, j, x1, x2;
+	int s, l1, l2, r1, r2, i, j, x1, x2;
 	cin >> s >> l1 >> r1 >> l2 >> r2;
-	x1 = 0;
-	x2 = s + 20;
-	for (i = l1; i <= r1; i++)
+	for (x1 = l1; x1 <= r1; x1++)
 	{
-		for (j = l2; j <= r2; j++)
+		for (x2 = l2; x2 <= r2; x2++)
 		{
-			if (i + j == s)
-			{
-				x1 = i;
-				x2 = j;
-
-			}
+		
 			if (x1 + x2 == s)
 			{
 				cout << x1 << "\t" << x2 << "\n";
